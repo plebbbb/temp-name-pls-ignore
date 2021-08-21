33895004c02ee44caa653577cd9805f37b2a25e3
plebbbb <genericpersonyoudontknow@gmail.com>
2021-08-21 14:27:32 -0400

    update decode.py

--- PYTHON/decode.py
+++ PYTHON/decode.py
@@ -3,7 +3,6 @@ import conversions
 ##sys.argv[] is how you get your parameters
 ##just print stuff, do sys.stdout.flush() at end
 
-#tringdata = conversions.letterstring_to_bytestring(str(sys.argv[1]))
 finaldata = conversions.mask_key(conversions.letterstring_to_bytestring(str(sys.argv[1])), str(sys.argv[2]))
 sys.stdout.write(finaldata)
 sys.stdout.flush()
